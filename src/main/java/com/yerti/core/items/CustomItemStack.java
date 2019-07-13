4ca052953e5c7535ec52b8e10bca5998371b442a
UZ9 <chessboy5@icloud.com>
2019-07-13 17:31:02 -0500

    Added extra ItemStack option

--- src/main/java/com/yerti/core/items/CustomItemStack.java
+++ src/main/java/com/yerti/core/items/CustomItemStack.java
@@ -9,16 +9,25 @@ import org.bukkit.inventory.ItemStack;
 public class CustomItemStack extends ItemStack {
 
 
+    public CustomItemStack(Material type, int amount) {
+        this(type, amount, null, false);
+    }
+
+
     public CustomItemStack(Material type, int amount, String displayName, boolean glowing) {
         this.setType(type);
+        if (displayName != null) {
+            this.getItemMeta().setDisplayName(displayName);
+        }
         this.setAmount(amount);
-        this.getItemMeta().setDisplayName(displayName);
+
 
         if (glowing) {
             this.addEnchantment(Enchantment.DIG_SPEED, 1);
             this.getItemMeta().addItemFlags(ItemFlag.HIDE_ENCHANTS);
         }
     }
+
 
 
 
