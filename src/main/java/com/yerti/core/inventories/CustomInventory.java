e1c60b6a79324e8cca1a3558eb20c2e7bb7b6981
UZ9 <chessboy5@icloud.com>
2019-07-13 19:29:31 -0500

    Added a CustomInventory for easily filling the background of the inventory with an item of my choosing

---
+++ src/main/java/com/yerti/core/inventories/CustomInventory.java
@@ -0,0 +1,61 @@
+package com.yerti.core.inventories;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+public class CustomInventory implements InventoryHolder {
+
+    Inventory inventory;
+    private int slots;
+    private String displayName;
+    private ItemStack backgroundItem;
+
+
+
+    public CustomInventory(int slots, String displayName, ItemStack backgroundItem) {
+        this.slots = slots;
+        this.displayName = displayName;
+        this.backgroundItem = backgroundItem;
+
+        inventory = Bukkit.createInventory(this, slots, displayName);
+    }
+
+    public void createBackground() {
+        for (int i = 0; i < slots; i++) {
+            if (inventory.getContents()[i] == null) {
+                inventory.setItem(i, backgroundItem);
+            }
+        }
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+
+    public int getSlots() {
+        return slots;
+    }
+
+    public void setSlots(int slots) {
+        this.slots = slots;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public ItemStack getBackgroundItem() {
+        return backgroundItem;
+    }
+
+    public void setBackgroundItem(ItemStack backgroundItem) {
+        this.backgroundItem = backgroundItem;
+    }
+}
