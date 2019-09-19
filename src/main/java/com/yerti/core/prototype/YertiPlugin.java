2929594bc9f7b61e694015ef517d8042b25e15ed
UZ9 <chessboy5@icloud.com>
2019-09-18 21:07:05 -0500

    One more javadoc comment

--- src/main/java/com/yerti/core/prototype/YertiPlugin.java
+++ src/main/java/com/yerti/core/prototype/YertiPlugin.java
@@ -3,8 +3,15 @@ package com.yerti.core.prototype;
 import com.yerti.core.prototype.command.CommandFramework;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Class for loading any additional resources with only calling one method
+ * Main class extends YertiPlugin (which then extends JavaPlugin
+ */
 public class YertiPlugin extends JavaPlugin {
 
+    /**
+     * Loads needed resources
+     */
     protected void load() {
         new CommandFramework(this);
     }
