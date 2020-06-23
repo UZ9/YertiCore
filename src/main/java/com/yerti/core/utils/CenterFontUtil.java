aa4bab0455719a3ca175d8b6df929bc6786b1387
UZ9 <chessboy5@icloud.com>
2020-06-22 19:51:40 -0500

    Added text centering util

---
+++ src/main/java/com/yerti/core/utils/CenterFontUtil.java
@@ -0,0 +1,44 @@
+package com.yerti.core.utils;
+
+import org.bukkit.ChatColor;
+
+//Credit to https://www.spigotmc.org/threads/free-code-sending-perfectly-centered-chat-message.95872/, wonderful chap
+public class CenterFontUtil {
+
+    private final static int CENTER_PX = 154;
+
+    public static String centerString(String message) {
+        message = ChatColor.translateAlternateColorCodes('&', message);
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for(char c : message.toCharArray()){
+            if(c == '§'){
+                previousCode = true;
+                continue;
+            }else if(previousCode){
+                previousCode = false;
+                isBold = c == 'l' || c == 'L';
+            }else{
+                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while(compensated < toCompensate){
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+
+        return sb.toString() + message;
+    }
+
+}
