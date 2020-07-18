befe747774a28583f09a72cd93cb7c2c8fcee793
UZ9 <chessboy5@icloud.com>
2020-07-17 20:59:55 -0500

    Fix potential NPE with PersistentBlockStorage and update pom

--- src/main/java/com/yerti/core/block/storage/PersistentBlockStorage.java
+++ src/main/java/com/yerti/core/block/storage/PersistentBlockStorage.java
@@ -1,17 +1,29 @@
 package com.yerti.core.block.storage;
 
+import com.yerti.core.logging.Logger;
+import com.yerti.core.utils.LocationUtils;
 import org.bukkit.Location;
 
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
 public abstract class PersistentBlockStorage {
     private Map<Location, Map<String, Serializable>> data = new HashMap<>();
 
-    public void storeValue(Location location, String key, Serializable value) { data.get(location).put(key, value); }
+    public void storeValue(Location location, String key, Serializable value) {
+        if (!data.containsKey(location)) data.put(location, new HashMap<>());
+
+        data.get(location).put(key, value);
+    }
 
     public Object getValue(Location location, String key) {
+        if (!data.containsKey(location) || !data.get(location).containsKey(key)) {
+            Logger.log(Level.SEVERE,  "Couldn't find key " + key + " in location " + LocationUtils.serializeLocation(location));
+            return null;
+        }
+
         return data.get(location).get(key);
     }
 
